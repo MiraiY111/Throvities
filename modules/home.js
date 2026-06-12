4a6e979e855482ac09c6578ca8a5933a117ec40f
MiraiY111 <aryairvan123@gmail.com>
2026-06-12 19:17:39 +0700

    Delete modules/home.js

--- modules/home.js
+++
@@ -1,7 +0,0 @@
-// modules/home.js
-
-export function initHome() {
-    console.log("🏠 Home Module Active");
-    
-    // Kamu bisa menambahkan manipulasi DOM untuk rules atau data timeline server di sini nanti.
-}
